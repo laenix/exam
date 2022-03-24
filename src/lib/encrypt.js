c59b1fe6f2c4ad11f632fba3891bcd8d6babc9ce
bobo8658 <865839292@qq.com>
2022-03-24 21:06:39 +0800

    update

--- src/lib/encrypt.js
+++ src/lib/encrypt.js
@@ -1,10 +1,10 @@
+var exam_public_key = `-----BEGIN PUBLIC KEY-----
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDDLyh5Lb/5TprGvS9yFCcpCYnb
+0FuSyY3+TPbJI7Pv3+u4eFoqGGN46qyFOVLhUuFRttMfoA8h8yrdYCssLi93baoB
+yTMYf5/KVlviLKXWd3TDOJdeSX4d+qLUp/WK0ckm2VaJuY5vW0x5x6WbZ8MSxwTD
+MqNNMgVUdOgD3MIScwIDAQAB
+-----END PUBLIC KEY-----`;
 function encrypt(params) {
-    var exam_public_key = `-----BEGIN PUBLIC KEY-----
-        MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDDLyh5Lb/5TprGvS9yFCcpCYnb
-        0FuSyY3+TPbJI7Pv3+u4eFoqGGN46qyFOVLhUuFRttMfoA8h8yrdYCssLi93baoB
-        yTMYf5/KVlviLKXWd3TDOJdeSX4d+qLUp/WK0ckm2VaJuY5vW0x5x6WbZ8MSxwTD
-        MqNNMgVUdOgD3MIScwIDAQAB
-        -----END PUBLIC KEY-----`;
     var encrypt = new JSEncrypt();
     encrypt.setPublicKey(exam_public_key);
     var encrypted = encrypt.encrypt(params);
